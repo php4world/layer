0e7099d975fe66284cc3b3353ebb01c70d3b6daa
php4world <76422937@qq.com>
2019-07-11 08:42:23 +0800

    vuepress use plugin

---
+++ docs/.vuepress/components/layer-run.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="run">运行代码</button>
+</template>
+
+<script>
+    import Vue from 'vue';
+    export default {
+        mounted() {
+            import('@php4world/layer/dist/layer.umd.js').then(module => {
+                Vue.use(module.default);
+            });
+        },
+        methods: {
+            run() {
+                this.$layer.alert('111');
+            }
+        }
+    };
+</script>
